2e3f1b8afce6675dfa3ccf279a89c3f4cb38d53a
SagarTeotia1 <sagar1teotia@gmail.com>
2025-10-17 22:38:31 +0530

    commit
    commit

--- apps/api-gateway/jest.config.ts
+++ apps/api-gateway/jest.config.ts
@@ -10,7 +10,7 @@ const swcJestConfig = JSON.parse(
 swcJestConfig.swcrc = false;
 
 export default {
-  displayName: '@backendcampusmart/my-new-app',
+  displayName: '@backendcampusmart/api-gateway',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
